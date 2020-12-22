729d7846f2515c05080ee0cc94626518a2c6a6c7
monkcs <charlie.habolin@monkcs.se>
2020-12-22 21:11:09 +0100

    New update

--- javascript/sitescript.js
+++ javascript/sitescript.js
@@ -1,6 +1,5 @@
 /* Set language based on user agent language */
-if (navigator.language.includes("sv"))
-{
+if (navigator.language.includes("sv")) {
     document.documentElement.setAttribute('lang', 'sv');
 }
 
@@ -11,31 +10,24 @@ load(window.location.hash.substring(1) + window.location.search, true);
 
 fetch("/post/register--refresh.html").then(validate).then(sidepanel);
 
-window.addEventListener('popstate', function (event)
-{
-    if (event.state)
-    {
+window.addEventListener('popstate', function (event) {
+    if (event.state) {
         shellstyle(true, true);
         load(event.state.path, true);
     }
 }, false);
 
-function events()
-{
-    for (let link of document.getElementsByTagName("a"))
-    {
-        if (link.pathname.startsWith("/post/") || link.pathname.startsWith("/authors/"))
-        {
-            if (link.getAttribute("download") == null)
-            {
+function events() {
+    for (let link of document.getElementsByTagName("a")) {
+        if (link.pathname.startsWith("/post/") || link.pathname.startsWith("/authors/")) {
+            if (link.getAttribute("download") == null) {
                 link.addEventListener("click", relay);
             }
         }
     }
 }
 
-function relay(event)
-{
+function relay(event) {
     event.preventDefault();
 
     document.getElementById("navigation-toggle").checked = false;
@@ -44,37 +36,30 @@ function relay(event)
     load(path, false);
 }
 
-function load(url, replace)
-{
-    if (url.startsWith("/post/"))
-    {
+function load(url, replace) {
+    if (url.startsWith("/post/")) {
         var path = {
             path: url
         };
 
-        if (replace)
-        {
+        if (replace) {
             history.replaceState(path, null, url);
         }
-        else
-        {
+        else {
             history.pushState(path, null, url);
         }
 
         fetch(url).then(validate).then(insert).catch(offline);
     }
-    else
-    {
+    else {
         var path = {
             path: "/"
         };
 
-        if (replace)
-        {
+        if (replace) {
             history.replaceState(path, null, "/");
         }
-        else
-        {
+        else {
             history.pushState(path, null, "/");
         }
 
@@ -82,60 +67,47 @@ function load(url, replace)
     }
 }
 
-function validate(response)
-{
-    if (response.ok)
-    {
+function validate(response) {
+    if (response.ok) {
         return response.text();
     }
-    else
-    {
+    else {
         throw new Error('Network response was not valid');
     }
 }
 
-function offline()
-{
+function offline() {
     fetch("/post/offline/").then(validate).then(insert);
 }
 
-function insert(html)
-{
-    for (let article of document.getElementsByTagName("article"))
-    {
+function insert(html) {
+    for (let article of document.getElementsByTagName("article")) {
         article.remove();
     }
 
-    if (html.includes('data-shellstyle="hide"'))
-    {
+    if (html.includes('data-shellstyle="hide"')) {
         shellstyle(true, false);
     }
-    if (html.includes('data-shellstyle="purge"'))
-    {
+    if (html.includes('data-shellstyle="purge"')) {
         shellstyle(false, false);
     }
 
-    try
-    {
-        for (let main of document.getElementsByTagName("main"))
-        {
+    try {
+        for (let main of document.getElementsByTagName("main")) {
             main.insertAdjacentHTML("beforeend", html);
         }
     }
-    catch (message)
-    {
+    catch (message) {
         console.error("Not possible to get refrence of 'main' node, inserting into 'body' instead");
 
-        for (let body of document.getElementsByTagName("body"))
-        {
+        for (let body of document.getElementsByTagName("body")) {
             body.insertAdjacentHTML("beforeend", html);
         }
     }
 
     events();
 
-    try
-    {
+    try {
         /* Try to import additional javascript from article directory */
         import(window.location.pathname + 'javascript/script.js').then(module => module.default()).catch(error => { });
     }
@@ -143,27 +115,22 @@ function insert(html)
 }
 
 var bodystorage = null;
-function shellstyle(shell, style)
-{
+function shellstyle(shell, style) {
     // Enable or disable the shell by moving it to and from a template node.
     // This enable us to remove the shell if a web application wants to use the
     // whole page without a chrome. The web application can also choose to keep
     // or disable the stylesheets to.
-    if (!shell)
-    {
+    if (!shell) {
         let body = document.getElementsByTagName("body")[0];
         bodystorage = body.cloneNode(true);
 
         // Remove the body node and all its children.
-        while (body.firstChild)
-        {
+        while (body.firstChild) {
             body.removeChild(body.firstChild);
         }
     }
-    else
-    {
-        if (bodystorage != null)
-        {
+    else {
+        if (bodystorage != null) {
             // Restore the body node and clear the temporary storage.
             let body = document.getElementsByTagName("body")[0];
             body.replaceWith(bodystorage);
@@ -171,18 +138,14 @@ function shellstyle(shell, style)
         }
     }
 
-    for (let stylesheet of document.getElementsByClassName("main-stylesheet"))
-    {
+    for (let stylesheet of document.getElementsByClassName("main-stylesheet")) {
         stylesheet.disabled = !style;
     }
 }
 
-function sidepanel(html)
-{
-    try
-    {
-        for (let node of document.getElementById("article-register").children)
-        {
+function sidepanel(html) {
+    try {
+        for (let node of document.getElementById("article-register").children) {
             node.remove();
         }
 
